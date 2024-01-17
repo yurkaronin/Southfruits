b974fd7d929ebdcb290c40e9b2a55d68028f27d9
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-01-17 09:42:40 +0300

    Добавил вёрстку мобильного меню

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ function requestTick() {
   ticking = true;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   headerChange();
   window.addEventListener("scroll", onScroll, { passive: true });
 
@@ -35,23 +35,52 @@ document.addEventListener('DOMContentLoaded', function() {
   const links = document.querySelectorAll('.header__navigation a');
 
   links.forEach(link => {
-      // Обработчик для события наведения мыши на ссылку
-      link.addEventListener('mouseenter', () => {
-          // Удаляем класс 'off' со всех ссылок перед добавлением новых
-          links.forEach(l => l.classList.remove('off'));
+    // Обработчик для события наведения мыши на ссылку
+    link.addEventListener('mouseenter', () => {
+      // Удаляем класс 'off' со всех ссылок перед добавлением новых
+      links.forEach(l => l.classList.remove('off'));
 
-          // Добавляем класс 'off' только предыдущему элементу, если он существует
-          if (link.previousElementSibling) {
-              link.previousElementSibling.classList.add('off');
-          }
-      });
+      // Добавляем класс 'off' только предыдущему элементу, если он существует
+      if (link.previousElementSibling) {
+        link.previousElementSibling.classList.add('off');
+      }
+    });
 
-      // Обработчик для события, когда курсор мыши покидает ссылку
-      link.addEventListener('mouseleave', () => {
-          // Удаляем класс 'off' со всех ссылок
-          links.forEach(l => l.classList.remove('off'));
-      });
+    // Обработчик для события, когда курсор мыши покидает ссылку
+    link.addEventListener('mouseleave', () => {
+      // Удаляем класс 'off' со всех ссылок
+      links.forEach(l => l.classList.remove('off'));
+    });
   });
+
+  // Аккордеончик
+
+  // Находим все элементы с классом accordion__head
+  const accordionHeaders = document.querySelectorAll('.accordion__head');
+
+  // Добавляем обработчик событий для каждого заголовка аккордеона
+  accordionHeaders.forEach(header => {
+    header.addEventListener('click', function (event) {
+      // Находим ближайший родительский элемент с классом accordion
+      const accordion = event.target.closest('.accordion');
+
+      // Переключаем класс active для данного аккордеона
+      if (accordion) {
+        accordion.classList.toggle('active');
+      }
+    });
+  });
+
+  // показ мобильного меню и кнопки
+  const buttonMenu = document.querySelector('.button-menu');
+  const navigation = document.querySelector('.mob-nav');
+  buttonMenu.addEventListener('click', function () {
+    buttonMenu.classList.toggle('isActive');
+    navigation.classList.toggle('isActive');
+    document.body.classList.toggle('menu-open');
+  });
+
+
 });
 
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
