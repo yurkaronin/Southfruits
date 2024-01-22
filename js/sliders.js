39fcee456c40fb2e62c19e10a4bd98bbec1562c1
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-01-22 16:41:12 +0300

    Завершаю работу над внутренними страницами

--- js/sliders.js
+++ js/sliders.js
@@ -12,7 +12,24 @@ if(document.querySelector('.hero-slider .swiper')) {
       prevEl: ".hero-slider .swiper-button-prev",
     },
   });
-}
+};
+
+var swiperPhoto = new Swiper(".photo-slider .swiper", {
+  slidesPerView: 1,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: ".photo-slider .swiper-button-next",
+    prevEl: ".photo-slider .swiper-button-prev",
+  },
+  breakpoints: {
+    // Когда ширина экрана меньше 1024px
+    1024: {
+      slidesPerView: 2, // Показывать только 1 слайд
+      spaceBetween: 16 // Можно изменить промежуток, если нужно
+    }
+  }
+});
+
 
 
 });
